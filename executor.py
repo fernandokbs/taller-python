4912cbb6f54417ae8ecb4938dc2d8fc11eb36e3e
Fernando <fernando93d@gmail.com>
2025-02-13 18:16:42 -0600

    Fix

--- executor.py
+++ executor.py
@@ -5,6 +5,27 @@ import sys
 import argparse
 import subprocess
 import paramiko
+from celery import Celery
+
+app = Celery('tasks', broker='redis://localhost:6379/0')
+
+@app.task
+def exec_command(script):
+    try:
+        client = paramiko.SSHClient()
+        client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+        client.connect(
+            hostname="10.176.186.145",
+            username="ubuntu",
+            port="22"
+        )
+
+        client.exec_command(script)
+    except Exception as e:
+        print(f"Error al conectar: {e}")
+    finally:
+        client.close()
 
 class Executor:
     def __init__(self, script_name, args):
@@ -16,16 +37,7 @@ class Executor:
         self.execute(template)
 
     def execute(self, script):
-        client = paramiko.SSHClient()
-        client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-
-        client.connect(
-            hostname=os.getenv('HOST'),
-            username=os.getenv('REMOTE_USER'),
-            port=os.getenv('PORT')
-        )
-
-        client.exec_command(script)
+        exec_command.delay(script)
 
     def get_template(self):
         env = Environment(loader=FileSystemLoader("scripts"))
